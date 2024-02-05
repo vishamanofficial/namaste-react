6154c4770b1de157967fa524cca7ff3bff70c3a4
Aman Vishwakarma <vishamanofficial@gmail.com>
2024-02-05 15:08:53 +0530

    namaste react

--- Episode03/App.js
+++ Episode03/App.js
@@ -2,7 +2,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// jb hm React.createElement krte hai to wo ek react elemant hota hai naa ki html element aur ye react elemenet  hota hai ji ki ek object hai aur fir jb hm ise dom se render krte hain to html element create hota hai 
+// jb hm React.createElement krte hai to wo ek react elemant hota hai naa ki html element aur ye react elemenet  hota hai jo ki ek object hai aur fir jb hm ise dom se render krte hain to html element create hota hai 
 
 // const heading = React.createElement("h1", { id:"heading"}, "namaste react");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -27,8 +27,8 @@ import ReactDOM from "react-dom/client";
 //     <h1>This is title component</h1>
 //     );
 
+
 // const HeadingComponent = () => (
-    
 //     <div id="container">
 //         < Title />
 //         {/* this is component composition */}
@@ -41,18 +41,23 @@ import ReactDOM from "react-dom/client";
 // root.render(<HeadingComponent />);
 
 
-// --------------------------------------------------------------
+// koi bhi function jo react element ya jsx element return karega , functional component kahlayega --------------------------------------------------------------
 
 // you can use any javascript thing in functional component inside { anything like var, function }
 
-const Title = (
+const Title = () => (
     <h1>This is title component</h1>
+    );
+
+const Para = () => (
+    <p>this is para</p>
     );
 
 const HeadingComponent = () => (
     
     <div id="container">
         {Title}
+        {Para}
         <h1>Namaste React Functional Component</h1>
     </div>
 );
@@ -60,4 +65,7 @@ const HeadingComponent = () => (
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<HeadingComponent />);
+
+// this is called component composition     means using one componenet in another component
+// can be asked in interview
 
