5c8707d41ff34b4228904ab280a27dee7aa4c33b
Aman Vishwakarma <vishamanofficial@gmail.com>
2024-02-20 12:39:36 +0530

    namaste react

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -4,6 +4,13 @@ class UserClass extends React.Component {
     constructor(props){
         super(props)
 
+        // creating state inside class based component
+        // it contains all state variable
+        this.state = {
+            count:0,
+            // count2:1,
+        };
+
     }
     render() {
         return (
@@ -12,6 +19,8 @@ class UserClass extends React.Component {
                 <h2>Name : {this.props.name}</h2>
                 <h3>Location : Lucknow</h3>
                 <h4>Contact : @vishamanofficial</h4>
+                <h5>Count : {this.state.count}</h5>
+                {/* <h5>Count2 : {this.state.count2}</h5> */}
             </div>
         );
     }
